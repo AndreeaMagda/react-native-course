160feb9ebbdf558f843d90b776add077bdf26eb2
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2025-01-23 20:21:31 +0200

    Image component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export default function HomeScreen() {
   return (
@@ -7,6 +7,12 @@ export default function HomeScreen() {
         The <Text style={{ fontWeight: 'bold' }}>quick brown fox </Text>jumps
         over the lazy dog
       </Text>
+      <Image
+        style={{ width: 100, height: 100, marginTop: 20 }}
+        source={{
+          uri: 'https://www.italpassion.fr/wp-content/uploads/2024/12/charles-leclerc-ferrari.jpg',
+        }}
+      />
     </View>
   );
 }
