f2d5d7868e5c7015ff089e6cf23b60515a46b2b1
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2025-01-23 20:30:21 +0200

    ScrollView component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,17 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text style={{ fontSize: 16, color: 'purple' }}>
-        The <Text style={{ fontWeight: 'bold' }}>quick brown fox </Text>jumps
-        over the lazy dog
-      </Text>
-      <Image
-        style={{ width: 100, height: 100, marginTop: 20 }}
-        source={{
-          uri: 'https://www.italpassion.fr/wp-content/uploads/2024/12/charles-leclerc-ferrari.jpg',
-        }}
-      />
+      <Text style={{ fontSize: 24, textAlign: 'center' }}>Scroll me!</Text>
+      <View style={{ height: 400, backgroundColor: '#e5e5e5' }}>
+        {/* This is our scrollable area */}
+        <ScrollView>
+          <View style={{ width: 300, height: 300, backgroundColor: 'red' }} />
+          <View style={{ width: 300, height: 300, backgroundColor: 'green' }} />
+          <View style={{ width: 300, height: 300, backgroundColor: 'blue' }} />
+        </ScrollView>
+      </View>
     </View>
   );
 }
