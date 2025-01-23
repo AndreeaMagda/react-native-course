a7954790dccee3871a33c093d7221ec0f66b46db
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2025-01-23 21:22:31 +0200

    TextInput component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,25 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import React, { useState } from 'react';
 
 export default function HomeScreen() {
-  const [pressedCount, setPressedCount] = useState(0);
+  const [name, setName] = useState('');
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ margin: 16, fontSize: 16, textAlign: 'center' }}>
-        {pressedCount > 0
-          ? `The button was pressed ${pressedCount} times!`
-          : "The button isn't pressed yet"}
+    <View
+      style={{
+        flex: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
+        padding: 16,
+      }}
+    >
+      <Text style={{ marginVertical: 16 }}>
+        {name ? `Hi ${name}!` : 'What is your name?'}
       </Text>
-      <Button
-        title="Press me"
-        onPress={() => setPressedCount(pressedCount + 1)}
-        disabled={pressedCount > 3}
+      <TextInput
+        style={{ padding: 8, backgroundColor: '#f5f5f5' }}
+        onChangeText={(text) => setName(text)}
+        secureTextEntry
       />
     </View>
   );
