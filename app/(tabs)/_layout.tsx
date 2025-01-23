b0845f024ad8a97ae1243327e3bf2869708f9844
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2025-01-23 19:56:26 +0200

    Text Component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,6 +8,11 @@ import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+//render some text in the center of our screen
+//making our font-size 16 pixels.
+//setting our text color to purple.
+//making our text bold by wrapping it in a nested Text component..
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
@@ -25,19 +30,15 @@ export default function TabLayout() {
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
         }}
       />
     </Tabs>
