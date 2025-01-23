6302aabedd8c0f81b497a4560946769ca9e22294
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2025-01-23 21:37:06 +0200

    Combining components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,18 @@
-import { View, Text, TextInput } from 'react-native';
-import React, { useState } from 'react';
+import { View } from 'react-native';
+import React from 'react';
 
 export default function HomeScreen() {
-  const [name, setName] = useState('');
-
   return (
-    <View
-      style={{
-        flex: 1,
-        alignContent: 'center',
-        justifyContent: 'center',
-        padding: 16,
-      }}
-    >
-      <Text style={{ marginVertical: 16 }}>
-        {name ? `Hi ${name}!` : 'What is your name?'}
-      </Text>
-      <TextInput
-        style={{ padding: 8, backgroundColor: '#f5f5f5' }}
-        onChangeText={(text) => setName(text)}
-        secureTextEntry
-      />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Box color="pink" />
+      <Box color="purple" />
+      <Box color="navy" />
     </View>
   );
 }
+
+export const Box = (props: { color: string }) => (
+  <View
+    style={{ width: 100, height: 100, backgroundColor: props.color, margin: 8 }}
+  />
+);
